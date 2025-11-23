b9db62740bae0179fc4467b3e219a3cd6859b611
mungaiDaniel <danitomonga@gmail.com>
2025-11-23 17:48:44 +0300

    newuryt

--- src/httpClient.js
+++ src/httpClient.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export default axios.create({
-    baseURL: "https://trina-overmild-cristobal.ngrok-free.dev",
+    baseURL: "http://127.0.0.1:5000",
     withCredentials: true,
 })
